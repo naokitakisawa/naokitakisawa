8283d433d53f242857b2746b8984e30507bd38e8
naokitakisawa <104759071+naokitakisawa@users.noreply.github.com>
2022-05-06 20:15:18 +0900

    Add files via upload
    1年でラッキーな日を列記するpythonプログラムを作りました。luchydays.py on GitHub
     
     
    六曜、
    十二直、
    一粒万倍日、
    天赦日、
    不成就日、
    寅の日、
    己巳の日、
    巳の日、
    二十八宿
    を表示します。

--- luckydays.py
+++ luckydays.py
@@ -17,12 +17,16 @@ import Ichiryuumanbainichi
 import Tenshanichi
 import Fujyoujyunichi
 import Nijyuuhassyuku
-
+import Jyushibi
+import Jyuunityoku
 
 print('不成就日とは、何事もうまくいかない、何をしても良い結果にならず願い事が叶わない縁起の悪い日（凶日）。\n')
+print('天赦日とは、天が万物の罪を赦す（許す）日で、最上の大吉日。年に5～6日しかないことから、「大安よりお日柄が良い」といわれる。')
 print('一粒万倍日とは、新しいことをスタートするのに良い日とされ、特に仕事始めや開業、種まき、お金の支出に吉。しかし借金や借りを作ることは、苦労の種が増えるのでよくないとされる。他の暦注と日が重なったとき、吉日と重なれば一粒万倍日の効果が倍増し、凶日と重なれば半減するという。\n')
 print('鬼宿日とは、二十八宿の「鬼」の日。大吉日、長寿や名誉を祝うのに殊に良い日。')
-
+print('受死日は「病を患えば必ず死ぬ最悪の大凶日」という扱いを受けており、一説には他の暦注を一切無視して凶日にしてしまうほどの力があると言われております。')
+print('寅の日は、干支の中でも最も金運に縁のある吉祥日。金運将来日。読み方は「とらのひ」。')
+print('己巳と巳')
 thisYear = dt.date.today().year
 
 
@@ -30,10 +34,12 @@ date_index = pd.date_range(start=str(thisYear) + '-01-01', end=str(thisYear) + '
 date_ary = date_index.to_series().dt.strftime("%Y-%m-%d")
 
 fujyou = Fujyoujyunichi.Fujyoujyunichi(thisYear)
+jyushibi = Jyushibi.Jyushibi(thisYear)
+jyuunityoku = Jyuunityoku.Jyuunityoku(thisYear)
 
 for a_date in date_ary.values:
-    y, m, d = a_date.split('-')
-    date = dt.date(int(y), int(m), int(d))
+    y, m, d = [int(a) for a in a_date.split('-')]
+    date = dt.date(y, m, d)
     
     # 曜日
     locale.setlocale(locale.LC_TIME, 'ja_JP.UTF-8')
@@ -43,15 +49,27 @@ for a_date in date_ary.values:
     events = ''
     
     # 六曜
-    k = Kyureki.from_ymd(int(y), int(m), int(d))
+    k = Kyureki.from_ymd(y, m, d)
     events += k.rokuyou
     
     
+    # 十二直
+    events += '、　' + jyuunityoku.jyuunityoku(a_date)
+    
+    # 二十八宿の鬼宿日
+    nijyuuhassyuku = Nijyuuhassyuku.nijyuuhassyukubi(a_date)
+    events += '、　' + nijyuuhassyuku + '宿'
+        
     # 不成就日
     if fujyou.is_fujyoujyunichi(a_date):
         events += '、　不成就日'
     
     
+    # 受死日
+    if jyushibi.is_jyushibi(a_date):
+        events += '、　受死日'
+        
+        
     # 天赦日
     tensyanichi = Tenshanichi.is_tensyanichi(a_date)
     if tensyanichi:
@@ -74,9 +92,7 @@ for a_date in date_ary.values:
     elif jyuunishi == '巳':
         events += '、　巳の日'
     
-    nijyuuhassyuku = Nijyuuhassyuku.nijyuuhassyukubi(a_date)
-    if nijyuuhassyuku == '鬼':
-        events += '、　鬼宿日'
+    
     
     
     
