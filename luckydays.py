110ec07a2f40b6946532535da95cbbe182796d96
naokitakisawa <104759071+naokitakisawa@users.noreply.github.com>
2022-05-07 10:43:39 +0900

    Add files via upload
    ラッキーな日を探す。
    
    不成就日とは、何事もうまくいかない、何をしても良い結果にならず願い事が叶わない縁起の悪い日（凶日）。
    受死日は「病を患えば必ず死ぬ最悪の大凶日」という扱いを受けており、一説には他の暦注を一切無視して凶日にしてしまうほどの力があると言われております。
    十死（じゅうしび、じゅうしにち）は十死一生日、天殺日などとも言う。受死日に次ぐ大悪日。何事にも悪い日とされ、葬式にも凶。
    天赦日とは、天が万物の罪を赦す（許す）日で、最上の大吉日。年に5～6日しかないことから、「大安よりお日柄が良い」といわれる。
    一粒万倍日とは、新しいことをスタートするのに良い日とされ、特に仕事始めや開業、種まき、お金の支出に吉。しかし借金や借りを作ることは、苦労の種が増えるのでよくないとされる。他の暦注と日が重なったとき、吉日と重なれば一粒万倍日の効果が倍増し、凶日と重なれば半減するという。
    
    鬼宿日とは、二十八宿の「鬼」の日。大吉日、長寿や名誉を祝うのに殊に良い日。
    寅の日は、干支の中でも最も金運に縁のある吉祥日。金運将来日。読み方は「とらのひ」。
    己巳と巳

--- luckydays.py
+++ luckydays.py
@@ -19,12 +19,14 @@ import Fujyoujyunichi
 import Nijyuuhassyuku
 import Jyushibi
 import Jyuunityoku
+import Jyuushinichi
 
 print('不成就日とは、何事もうまくいかない、何をしても良い結果にならず願い事が叶わない縁起の悪い日（凶日）。\n')
+print('受死日は「病を患えば必ず死ぬ最悪の大凶日」という扱いを受けており、一説には他の暦注を一切無視して凶日にしてしまうほどの力があると言われております。')
+print('十死（じゅうしび、じゅうしにち）は十死一生日、天殺日などとも言う。受死日に次ぐ大悪日。何事にも悪い日とされ、葬式にも凶。')
 print('天赦日とは、天が万物の罪を赦す（許す）日で、最上の大吉日。年に5～6日しかないことから、「大安よりお日柄が良い」といわれる。')
 print('一粒万倍日とは、新しいことをスタートするのに良い日とされ、特に仕事始めや開業、種まき、お金の支出に吉。しかし借金や借りを作ることは、苦労の種が増えるのでよくないとされる。他の暦注と日が重なったとき、吉日と重なれば一粒万倍日の効果が倍増し、凶日と重なれば半減するという。\n')
 print('鬼宿日とは、二十八宿の「鬼」の日。大吉日、長寿や名誉を祝うのに殊に良い日。')
-print('受死日は「病を患えば必ず死ぬ最悪の大凶日」という扱いを受けており、一説には他の暦注を一切無視して凶日にしてしまうほどの力があると言われております。')
 print('寅の日は、干支の中でも最も金運に縁のある吉祥日。金運将来日。読み方は「とらのひ」。')
 print('己巳と巳')
 thisYear = dt.date.today().year
@@ -56,9 +58,12 @@ for a_date in date_ary.values:
     # 十二直
     events += '、　' + jyuunityoku.jyuunityoku(a_date)
     
-    # 二十八宿の鬼宿日
+    # 二十八宿
     nijyuuhassyuku = Nijyuuhassyuku.nijyuuhassyukubi(a_date)
-    events += '、　' + nijyuuhassyuku + '宿'
+    if nijyuuhassyuku == '鬼':
+        events += '、　' + nijyuuhassyuku + '宿日'
+    else:
+        events += '、　' + nijyuuhassyuku
         
     # 不成就日
     if fujyou.is_fujyoujyunichi(a_date):
@@ -68,16 +73,16 @@ for a_date in date_ary.values:
     # 受死日
     if jyushibi.is_jyushibi(a_date):
         events += '、　受死日'
-        
+    
+    if Jyuushinichi.is_jyuushinichi(a_date):
+        events += '、　十死日'
         
     # 天赦日
-    tensyanichi = Tenshanichi.is_tensyanichi(a_date)
-    if tensyanichi:
+    if Tenshanichi.is_tensyanichi(a_date):
         events += '、　天赦日'
     
     # 一粒万倍日
-    chiryuumanbainichi = Ichiryuumanbainichi.is_ichiryuumanbainichi(a_date)
-    if chiryuumanbainichi:
+    if Ichiryuumanbainichi.is_ichiryuumanbainichi(a_date):
         events += '、　一粒万倍日'
         
     #十二支
@@ -85,7 +90,6 @@ for a_date in date_ary.values:
     if jyuunishi == '寅':
         events += '、　寅の日'
     
-    jyuunishi = Jyuunishi.jyuunishi_of_date(a_date)
     jikkan = Jikkan.jikkan_date(a_date)
     if jyuunishi == '巳' and jikkan == '己':
         events += '、　己巳の日'
